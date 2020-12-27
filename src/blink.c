2594612de8cd00b4f3866a6019b8891e0a115caa
Harris Ransom <hransom528@gmail.com>
2020-12-27 18:12:01 -0500

    Refactored code and updated it

--- src/blink.c
+++ src/blink.c
@@ -1,7 +1,16 @@
 #define DDRB  (*(volatile unsigned char*) 0x37)
 #define PORTB (*(volatile unsigned char*) 0x38)
+#define F_CPU 8000000UL // 8Mhz
+
+#include <util/delay.h>
 
 int main() {
-	DDRB |= 0b1;   // DB0 as output
-    PORTB |= 0b1;  // DB0 on   
+	DDRB |= 1; // DB0 as output
+    
+    while (1) {
+        PORTB |= 1;
+		_delay_ms(1000);
+        PORTB ^= 1;
+		_delay_ms(1000);
+	}
 }
